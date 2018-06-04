7ff51d82cf1ce679a2f110140cdb7343911b544a
Justin Grindal <justin.grindal@uipath.com>
2018-06-04 16:39:40 -0500

    Added IUncompressor and ZipUncompressor as basic framework for factory implementation

---
+++ UiPathTeam.File.Compression/ZipUncompressor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UiPathTeam.File.Compression
+{
+    class ZipUncompressor : IUncompressor
+    {
+        public void UncompressFile(string FilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
